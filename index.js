82f30b9fda640a831bcb6d15de1d7af09168b53b
Rajuprajapti <rprajuprajapati16@gmail.com>
2023-04-11 15:11:08 +0530

    backend with sql done

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv").config();
+const app = express();
+app.use(express.json());
+
+// Database name ,root,password
+const seq = new Sequelize("firstdb", "root", process.env.PASSWORD, {
+  host: "localhost",
+  //    name of database Type you are using.
+  dialect: "mysql",
+});
+
+// making schema.
+// todo:table you want to create.
+const todos = seq.define("todos", {
+  text: Sequelize.STRING,
+  status: Sequelize.BOOLEAN,
+});
+
+// controller
+app.get("/", async (req, res) => {
+  try {
+    const result = await todos.findAll();
+    res.status(200).json({ isError: false, result });
+  } catch (err) {
+    res.status(500).json({ message: "error while getting", isError: true });
+  }
+});
+
+app.post("/", async (req, res) => {
+  console.log("I am in post");
+  try {
+    const result = await todos.create(req.body);
+    console.log("RESult", result);
+    res.status(201).json({ isError: false, result });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ isError: err.message, message: "error while posting.." });
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
+    const result = await todos.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ isError: false, result });
+  } catch (err) {
+    res.status(500).json({ isError: true, message: "error while getting" });
+  }
+});
+
+// CONNECTING TO DB WILL GIVE PROMISE.
+seq.sync().then(() => {
+  app.listen(5000, () => {
+    console.log("listening on port 5000");
+  });
+});
